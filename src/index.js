db510f2cb6b742e20403fd79cd4a7197506b5c55
c-montgomery <craym87@gmail.com>
2021-08-19 11:57:43 -0700

    added stylesheet

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 //import whatever from date-fns
+import './style.css';
+
+function addContent() {
+    const div = document.createElement('div');
+    div.textContent = 'TODOLISTCSS working'
+    return div;
+}
+
+document.body.appendChild(addContent())
