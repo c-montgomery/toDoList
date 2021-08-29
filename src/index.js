ce39debba2ee3c73626e680a4cdd1450dadda26b
c-montgomery <craym87@gmail.com>
2021-08-29 00:09:27 -0700

    fixed goal replication bug, added delete button

--- src/index.js
+++ src/index.js
@@ -1,125 +1,127 @@
 
 import './style.css';
-import {DatePicker} from '../node_modules/date-time-picker-component/dist/js/date-time-picker-component.min'
+import { DatePicker } from '../node_modules/date-time-picker-component/dist/js/date-time-picker-component.min'
 
 const fart = 'fart'
 function addContent() {
     const div = document.createElement('div');
     div.classList = 'content';
-  
+
     return div;
 }
 
 
 document.body.appendChild(addContent())
 
-function createHeader(){
+function createHeader() {
     let header = document.createElement('header');
     header.textContent = 'To Do';
     let button = document.createElement('button');
     button.textContent = '+';
-    button.addEventListener('click', ()=>{
+    button.addEventListener('click', () => {
         const goalModal = modal()
         goalModal.createGoal('goalModal');
     })
     return [header, button]
 }
 
-const modal = () =>{
-    
-    const createGoal = (type) =>{
-        if (!document.querySelector('.goalModal')){
+const modal = () => {
+
+    const createGoal = (type) => {
+        if (!document.querySelector('.goalModal')) {
             let modal = document.createElement('div');
-            
+
             modal.textContent = 'Enter a goal title';
             modal.classList = type;
-    
+
             let exitModalButton = createButton('div', 'modalButton exit', 'X')
             exitModalButton.addEventListener('click', closeModal)
-    
+
 
             let saveModalButton = createButton('div', 'modalButton save', 'save')
-            saveModalButton.addEventListener('click', ()=>{
-               const what = handleDOM()
-               what.makeObject(nameInput.value)
-               closeModal();
-               what.refreshDOM();
+            saveModalButton.addEventListener('click', () => {
+                const what = handleDOM()
+                what.makeObject(nameInput.value)
+                closeModal();
+                what.refreshDOM();
             })
-    
+
             modal.appendChild(saveModalButton)
             modal.appendChild(exitModalButton)
-    
+
             let html = document.querySelector('html');
             html.classList = 'darkened';
-    
+
             let nameInput = document.createElement('input');
             nameInput.placeholder = 'Goal title'
             modal.appendChild(nameInput);
             console.log()
-            
+
             const content = document.querySelector('.content');
             content.appendChild(modal)
 
 
         }
-    
-        
+
+
     }
-    return {createGoal}
+    return { createGoal }
 };
-const refreshDOM = () =>{
 
-
-} 
-
-const handleDOM = (innerText) =>{
-    const toDoList = []
+const toDoList = []
+const handleDOM = (innerText) => {
+    let para = document.querySelectorAll('p');
+    para.forEach(p => p.remove())
     const refreshDOM = () => {
         toDoList.forEach(item => {
             let goalItem = document.createElement('p');
             goalItem.classList = 'goalItem'
             goalItem.textContent = item.name
-            
+
+            let deleteButton = document.createElement('button')
+            deleteButton.textContent = 'Delete'
+            goalItem.appendChild(deleteButton)
             let content = document.querySelector('body');
             content.append(goalItem)
             console.log(toDoList)
             console.log(item.name);
         })
-        console.log('hey, this should refressssh' )
+        console.log('hey, this should refressssh')
     }
-    const makeObject = (innerText) =>{
-        let goal = {name: innerText}
+    const makeObject = (innerText) => {
+        let goal = { name: innerText }
         toDoList.push(goal);
 
-        return {goal}
-        
-    
+        return { goal }
+
+
     }
-return {makeObject, refreshDOM}    
+    return { makeObject, refreshDOM }
 }
 
-function createButton(type, className, textContent=''){
+function createButton(type, className, textContent = '') {
     let button = document.createElement(type)
     button.classList = className
     button.textContent = textContent;
     return button;
-    
+
 }
 
-function closeModal(){
+function closeModal() {
     let deleted = document.querySelector('.goalModal');
     let html = document.querySelector('html');
     html.classList = '';
     deleted.remove()
 }
 
-function appendToDom(thing){
+function appendToDom(thing) {
     let div = document.querySelector('.content');
-    thing.forEach(element => { div.appendChild(element);
+    thing.forEach(element => {
+        div.appendChild(element);
         console.log(element)
     })
 }
-const saveObject = (innerText) =>{
+const saveObject = (innerText) => {
     console.log(innerText)
 }
 
@@ -128,7 +130,7 @@ const goalList = () => {
     const array = ['test'];
     const getArray = () => array;
     const addToArray = (object) => array.push(object)
-    return{goalList, getArray, addToArray}
+    return { goalList, getArray, addToArray }
 }
 
 
